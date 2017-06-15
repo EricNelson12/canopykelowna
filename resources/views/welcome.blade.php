1b5d6e67122fd4a2ca390ef4db43aa1719391caa
EricNelson12 <ericjnelson012@gmail.com>
2017-06-14 22:19:34 -0700

    Let's go!

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+@extends ('layout') @section('content')
+
+<div class="jumbotron text-center">
+    <h1>Canopy Kelowna</h1>
+    <p>Free range, premium, fresh edible crickets caught daily.</p>
+</div>
+<ul class="nav nav-tabs nav-justified">
+    <li class="active"><a href="#Home" data-toggle="tab">Home</a></li>
+    <li><a data-toggle="tab" href="#Volunteer">Volunteer</a></li>
+    <li><a data-toggle="tab" href="#Partnership">Partnership</a></li>
+    <li><a data-toggle="tab" href="#Order">Order</a></li>
+    <li><a data-toggle="tab" href="#Blog">Blog</a></li>
+
+  </ul>
+<div class="tab-content">
+    <div id="Home" class="container tab-pane fade in active">
+        <div class="row text-left img-responsive">
+            <div class="col-sm-4">
+                <img src="img/cricket.jpg" width="250" height="200" />
+                <h3>About </h3>
+                <p>Lorem ipsum dolor..</p>
+                <p>Ut enim ad..</p>
+            </div>
+            <div class="col-sm-8">
+                <h2>Our Mission: A more ethical food</h2>
+                <p>At Canopy Kelowna we suspect that you don't have a problem with killing bugs. If you do, then the following website is not for you, and we respect that!</p>
+                <p>Our goal is to function as an investigative body into the many aspects of edible bugs. For many <a href="https://www.vegansociety.com/go-vegan/why-go-vegan">reasons</a> we think that it is important to move away from using sentient animals
+                    like chickens, cows, and pigs as food. We think it is worthwhile to look into the possibility that traditional western meat sources can be replaced with crickets, mealworms, and mosquitos.</p>
+
+                <p>
+                    In particular we are hoping to understand and help advance the following aspects of an edible insect economy.
+                </p>
+
+                <h3>Psychology</h3>
+                <p>Our goal is to serve as a gateway for western civilization to enter the wonderful, bountiful and sustainable world of edible insects. We believe that food should be cheap, healthy and cause minimal damage to our environment.</p>
+                <p>
+                    <h3>Regulations</h3>
+                    <h3>Health</h3>
+                    <h3>Ethics</h3>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div id="Volunteer" class="tab-pane fade">
+      STUFF
+    </div>
+</div>
+
+
+
+@endsection
