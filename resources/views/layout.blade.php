6b645e03dfd9a688e9598a9847649619f7f2cb4f
EricNelson12 <ericjnelson012@gmail.com>
2017-06-15 00:00:01 -0700

    Added line to try and make mobile responsive with bootstrap 3

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href = "css/style.css" />
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Canopy Kelowna</title>
   </head>
   <body>
